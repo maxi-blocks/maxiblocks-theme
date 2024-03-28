80bea4ee160413883ef2179fd899373090b5e180
Svitlana <2401618+elzadj@users.noreply.github.com>
2024-03-28 18:29:55 +0200

    previews for EE block inserter

--- assets/src/admin/js/fse.js
+++ assets/src/admin/js/fse.js
@@ -66,22 +66,24 @@ document.addEventListener('DOMContentLoaded', function () {
                 
             } else {
                 // WordPress 6.5 fix
-                const previewGridDiv = document.querySelector('div.dataviews-view-grid');
-                if (!previewGridDiv) {console.log('Button not found yet, checking again...'); return;}
+                const previewGridDiv = document.querySelector('div.dataviews-view-grid') || document.querySelector('div.block-editor-block-patterns-list');
+                if (!previewGridDiv) {console.log('Grid not found yet, checking again...'); return;}
 
                 // Get all direct child divs with the class 'dataviews-view-grid__card'
-                const gridCards = previewGridDiv.querySelectorAll(':scope > .dataviews-view-grid__card');
-                if (!gridCards) {console.log('Button not found yet, checking again...'); return;}
+                const gridCards = previewGridDiv.querySelectorAll(':scope > .dataviews-view-grid__card, .block-editor-block-patterns-list__list-item');
+                if (!gridCards || gridCards.length === 0) {console.log('Card not found yet, checking again...'); return;}
+
+                console.log('Grid cards found.', gridCards);
 
                 gridCards.forEach(card => {
+                    if(card.classList.contains('maxiblocks-custom-pattern')) return;
+                    console.log('Checking card...');
+                    console.log(card.querySelector('div.block-editor-block-patterns-list__item')?.id);
                     const titleDiv = card.querySelector('div.edit-site-patterns__pattern-title');
-                    if (titleDiv) {
+                    const titleId = card.querySelector('div.block-editor-block-patterns-list__item')?.id
+                    if (titleDiv || titleId) {
                         // Get the text, convert to lowercase, and replace spaces with dashes
-                        const modifiedText = titleDiv.textContent.toLowerCase().replace(/\s+/g, '-');
-
-                        // Now you can manipulate the DOM as needed, for example:
-                      //  console.log(modifiedText); // Just an example of output
-
+                        const modifiedText = titleDiv ? titleDiv?.textContent?.toLowerCase().replace(/\s+/g, '-') : titleId.replace('maxiblocks/', '');
 
                         console.log('modifiedText', modifiedText);
                             console.log('idPart      ', idPart);
