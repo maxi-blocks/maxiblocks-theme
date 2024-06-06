d4052153cfea540934214af0e5023f49786addd5
Svitlana <2401618+elzadj@users.noreply.github.com>
2024-06-06 16:34:44 +0300

    Fix single post tempaltes bugs

--- maxi-patterns/all-archive-page-light-aaepl-pro-02/pattern.php
+++ maxi-patterns/all-archive-page-light-aaepl-pro-02/pattern.php
@@ -5,6 +5,7 @@
 
 return array(
     'title'	  => __('All Archive Page Light AAEPL-PRO-02', 'maxiblocks'),
+    'slug' => 'maxiblocks/all-archive-page-light-aaepl-pro-02',
     'categories' => array( 'mbt-all-archives' ),
     'templateTypes' => array('author', 'category', 'tag', 'date', 'archive', 'taxonomy'),
     'filePath' => __DIR__ . '/code.php',
