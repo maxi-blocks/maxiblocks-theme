02fd4a8e5b31de9640eab4bd459061d1ebe0d88b
Svitlana <2401618+elzadj@users.noreply.github.com>
2024-07-18 17:01:42 +0300

    Add basic patterns, parts and tempaltes

---
+++ maxi/patterns/basic-footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+  * Title: Basic Footer
+  * Slug: maxiblocks/basic-footer
+  * Categories: mbt-footer
+  * Block Types: core/template-part/footer
+  */
+?>
+<!-- wp:group {"className":"template_footer_columns template_width","layout":{"type":"default"}} -->
+<div class="wp-block-group template_footer_columns template_width"><!-- wp:columns {"className":"template_footer_columns","style":{"spacing":{"margin":{"top":"60px","bottom":"0"},"padding":{"bottom":"30px"}}}} -->
+<div class="wp-block-columns template_footer_columns" style="margin-top:60px;margin-bottom:0;padding-bottom:30px"><!-- wp:column {"width":"300px"} -->
+<div class="wp-block-column" style="flex-basis:300px"><!-- wp:heading -->
+<h2 class="wp-block-heading">Have any questions?</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"100%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:100%"><!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","justifyContent":"right"}} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"},"padding":{"bottom":"20px","top":"30px"}},"color":{"text":"#9b9b9b"},"elements":{"link":{"color":{"text":"#ff4a17"},":hover":{"color":{"text":"#c9340a"}}}}}} -->
+<p class="has-text-color has-link-color" style="color:#9b9b9b;margin-top:0;padding-top:30px;padding-bottom:20px">© Copyright – Made with&nbsp;<a href="https://maxiblocks.com/" target="_blank" rel="noreferrer noopener">Maxi Blocks</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
---
+++ patterns/basic-header.php
@@ -0,0 +1,27 @@
+<?php
+/**
+  * Title: Basic Header
+  * Slug: maxiblocks/basic-header
+  * Categories: mbt-header-navigation
+  * Block Types: core/template-part/header
+  */
+?>
+<!-- wp:columns {"className":"template_header_columns template_width","style":{"spacing":{"padding":{"top":"20px","bottom":"20px"},"margin":{"top":"0","bottom":"0"}}}} -->
+<div class="wp-block-columns template_header_columns template_width" style="margin-top:0;margin-bottom:0;padding-top:20px;padding-bottom:20px"><!-- wp:column {"verticalAlignment":"center","width":"390px"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:390px"><!-- wp:image {"id":631,"scale":"cover","sizeSlug":"full","linkDestination":"none","align":"center","className":"is-style-default"} -->
+<figure class="wp-block-image aligncenter size-full is-style-default"><img src="https://maxithemedev.dev600.com/wp-content/uploads/2024/07/maxi-logo.webp" alt="" class="wp-image-631" style="object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"100%","layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:100%"><!-- wp:navigation {"icon":"menu","style":{"spacing":{"blockGap":"2em"},"typography":{"fontSize":"16px"}},"layout":{"type":"flex","justifyContent":"center"}} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"330px","style":{"typography":{"fontStyle":"normal","fontWeight":"800"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-column" style="font-style:normal;font-weight:800;flex-basis:330px"><!-- wp:buttons {"layout":{"type":"flex","verticalAlignment":"center","justifyContent":"right"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"width":100,"className":"is-style-fill","style":{"color":{"background":"#000000"},"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"padding":{"left":"20px","right":"20px","top":"20px","bottom":"20px"}}},"fontSize":"medium"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 has-custom-font-size is-style-fill has-medium-font-size" style="font-style:normal;font-weight:700"><a class="wp-block-button__link has-background wp-element-button" style="background-color:#000000;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px">Say hello</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
