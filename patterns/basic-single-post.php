b488ea2bbc7ba8537a8fbec50dd6e02025197075
Svitlana <2401618+elzadj@users.noreply.github.com>
2024-09-11 18:29:09 +0300

    Fix author links and form styles

--- patterns/basic-single-post.php
+++ patterns/basic-single-post.php
@@ -11,5 +11,34 @@
 
 <!-- wp:post-featured-image {"style":{"border":{"radius":"30px"}}} /-->
 
-<!-- wp:post-content {"style":{"color":{"text":"#9b9b9b"},"elements":{"link":{"color":{"text":"#9b9b9b"},":hover":{"color":{"text":"#c9340a"}}}}}} /--></div>
+<!-- wp:post-content {"style":{"color":{"text":"#9b9b9b"},"elements":{"link":{"color":{"text":"#9b9b9b"},":hover":{"color":{"text":"#c9340a"}}}}}} /-->
+<!-- wp:comments -->
+<div class="wp-block-comments"><!-- wp:comments-title /-->
+
+<!-- wp:comment-template -->
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"40px"} -->
+<div class="wp-block-column" style="flex-basis:40px"><!-- wp:avatar {"size":40,"style":{"border":{"radius":"20px"}}} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:comment-author-name {"fontSize":"small"} /-->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"flex"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px"><!-- wp:comment-date {"fontSize":"small"} /-->
+
+<!-- wp:comment-edit-link {"fontSize":"small"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:comment-content /-->
+
+<!-- wp:comment-reply-link {"fontSize":"small"} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+<!-- /wp:comment-template -->
+
+<!-- wp:comments-pagination /-->
+
+<!-- wp:post-comments-form /--></div>
+<!-- /wp:comments --></div></div>
 <!-- /wp:group -->
